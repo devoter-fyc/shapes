e8646505637590fb80568591c6fb12428210e9c8
devoter-fyc <104010629+devoter-fyc@users.noreply.github.com>
2022-04-29 11:17:10 +0800

    Update lines.py

--- src/lines.py
+++ src/lines.py
@@ -1,1 +1,8 @@
-class
+class Line:
+    def __init__(self,length:int,pst='A',pend='B'):
+        self.__length = length
+        self.__points = [pst,pend]
+        return None
+    def __eq__(self,l):
+        return self.__length == l.__length and self.__points == l.__points
+    def __ne__

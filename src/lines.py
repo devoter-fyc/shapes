5e5cc1bf83eb2f6a7a4a85c0ade023dd459177d9
devoter-fyc <104010629+devoter-fyc@users.noreply.github.com>
2022-04-29 11:20:33 +0800

    Update lines.py

--- src/lines.py
+++ src/lines.py
@@ -7,4 +7,9 @@ class Line:
         return self.__length == l.__length and self.__points == l.__points
     def __ne__(self,l):
         return self.__length != l.__length or self.__points != l.__points
-    def 
+    def __lt__(self,l):
+        return self.__length < l.__length
+    def __gt__(self,l):
+        return self.__length > l.__length
+    def __le__(self,l):
+        return self.__length <= l.__length

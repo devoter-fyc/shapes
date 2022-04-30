a6a026c378937eea7010b593c2b0daab7a7e4234
devoter-fyc <104010629+devoter-fyc@users.noreply.github.com>
2022-04-30 16:36:54 +0800

    Update triangle.py

--- src/triangle.py
+++ src/triangle.py
@@ -3,3 +3,11 @@ class Triangle:
         self.__lengths = lengths
         self.__sides = 3
         return None
+    def __str__(self):
+        return f"Triangle object sides length {self.__sides}"
+    __repr__ = __str__
+    def __add__(self):
+        print("--------------------------INVALID OPERATION------------------------------")
+        raise ValueError from None
+        return None
+    __s
